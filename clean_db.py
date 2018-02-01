67ff628bf410e74034a7b772e5a93819a9069a90
piotrowskialek <piotrowskialek@icloud.com>
2018-02-01 14:33:37 +0100

    todo

--- clean_db.py
+++ clean_db.py
@@ -10,12 +10,15 @@ from functools import reduce
 
 class PsqlDao:
     def __init__(self):
-        self.conn = psycopg2.connect("dbname='komis' user='komis' host='localhost' password='komis'")
+        self.conn = psycopg2.connect("dbname='acn' user='acn' host='localhost' password='acn'")
         self.cur = self.conn.cursor()
 
     def insert_csv_file(self, file):
         cur = self.cur
-        csv_file = csv.reader(open(file), delimiter=';', quotechar='\'')
+        # csv_file = csv.reader(open(file), delimiter=';', quotechar='\'')
+        csv_file = open(file, 'r').read().split('\n')
+        columns = csv_file[0].split(';')
+        csv_file = csv_file[1:len(csv_file)]
         for csv_line in csv_file:
             line = str(reduce((lambda x, y: x + ';' + y), csv_line))
             if line.__contains__('id'):
