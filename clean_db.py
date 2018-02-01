a958a24ff0b4749077132385f7a8db85e7ca2ed7
piotrowskialek <piotrowskialek@icloud.com>
2018-02-01 19:07:30 +0100

    skrypt gotowy, teraz dane w csvkach

--- clean_db.py
+++ clean_db.py
@@ -1,35 +1,53 @@
 #!/usr/bin/python
-#pip install psycopg2 - jak nie dziala
+# pip install psycopg2 - jak nie dziala
 
 import psycopg2
 from os import listdir
-import csv
 import os
-from functools import reduce
-
+import sys
 
 class PsqlDao:
     def __init__(self):
-        self.conn = psycopg2.connect("dbname='acn' user='acn' host='localhost' password='acn'")
+        self.conn = psycopg2.connect("dbname='komis' user='komis' host='localhost' password='komis'")
         self.cur = self.conn.cursor()
 
     def insert_csv_file(self, file):
         cur = self.cur
-        # csv_file = csv.reader(open(file), delimiter=';', quotechar='\'')
         csv_file = open(file, 'r').read().split('\n')
-        columns = csv_file[0].split(';')
         csv_file = csv_file[1:len(csv_file)]
-        for csv_line in csv_file:
-            line = str(reduce((lambda x, y: x + ';' + y), csv_line))
-            if line.__contains__('id'):
-                pass #zapamietac
-            else:
-                pass #insert
+        for line in csv_file:
+            insert = "insert into {0} values(".format((file.split('.')[0]).split('/')[1])
+            line = line.replace(';', ',')
+            insert = insert + line + ");"
+            cur.execute(insert)
+            print "insert powiedzion'd"
 
 
 if __name__ == '__main__':
+    possible_exec = ["dict", "test"]
+
+    test = False
+    if len(sys.argv) != 2 or not (sys.argv[1] in possible_exec):
+        print "usage: ./clean_db <test/dict>"
+        exit(1)
+    elif sys.argv[1] == possible_exec[0]:
+        files = listdir("dictionary_data")
+    else:
+        files = listdir("test_data")
+        test = True
     dao = PsqlDao()
-    dict_data = './dictionary_data'
-    files = listdir(dict_data)
+    dao.cur.execute('begin')
     for file in files:
-        dao.insert_csv_file(os.path.join(dict_data, file))
+        if not test:
+            dao.insert_csv_file(os.path.join("dictionary_data", file))
+        else:
+            dao.insert_csv_file(os.path.join("test_data", file))
+    dao.cur.execute('commit')
+    print """ ____________________________
+< Zalanie danymi sie udalo xD >
+ ----------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\\
+                ||----w |
+                ||     ||"""
